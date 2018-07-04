3379bddd74ca47d8d5b908dc41e2974b17812139
tellomaxi <40808669+tellomaxi@users.noreply.github.com>
2018-07-04 20:44:18 -0300

    Add files via upload

--- 1-EntradaSalida/jsEntradaSalida-9.js
+++ 1-EntradaSalida/jsEntradaSalida-9.js
@@ -4,6 +4,15 @@ mostrar el importe con un aumento del 10 %
 en el cuadro de texto "RESULTADO".*/
 function mostrarAumento()
 {
+    var sueldo;
+    var aumento;
+    var resultado;
 
+    aumento = sueldo * 0.1;
+
+    sueldo = document.getElementById("sueldo").value;
+    sueldo = parseInt(sueldo);
+
+    document.getElementById("resultado").value = sueldo * 0.1 ;
 	
 }
