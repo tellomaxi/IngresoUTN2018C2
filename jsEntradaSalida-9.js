624357719011fdfc94207049eb44abf11a4bc7ec
tellomaxi <40808669+tellomaxi@users.noreply.github.com>
2018-07-04 20:47:24 -0300

    Add files via upload

---
+++ jsEntradaSalida-9.js
@@ -0,0 +1,19 @@
+/*Debemos lograr tomar el importe por ID ,
+transformarlo a entero (parseInt), luego
+mostrar el importe con un aumento del 10 %
+en el cuadro de texto "RESULTADO".*/
+function mostrarAumento()
+{
+    var sueldo;
+    var aumento;
+    var resultado;
+
+
+    sueldo = document.getElementById("sueldo").value;
+    sueldo = parseInt(sueldo);
+
+    aumento = sueldo * 0.1;
+    
+    document.getElementById("resultado").value = sueldo + aumento;
+	
+}
