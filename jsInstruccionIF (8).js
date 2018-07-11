2a8b06c6f05aa8be03ba66ab5f375876ab1dbf98
tellomaxi <40808669+tellomaxi@users.noreply.github.com>
2018-07-10 21:27:56 -0300

    Add files via upload

--- jsInstruccionIF (8).js
+++ jsInstruccionIF (8).js
@@ -2,12 +2,16 @@ function mostrar()
 {
 //tomo la edad  
 var edad;
-var estadoCivil
+var estadoCivil;
+
 edad = parseInt(document.getElementById("edad").value);
 estadoCivil = document.getElementById("estadoCivil").value;
 
-if (edad < 18 && estadoCivil != ("Soltero"))
+if (edad > 18 && estadoCivil == ("Soltero"))
 {
-    alert("Es muy pequeño para NO ser soltero");
+    alert("Es soltero y no es menor");
 }
+	
+
+
 }//FIN DE LA FUNCIÓN
