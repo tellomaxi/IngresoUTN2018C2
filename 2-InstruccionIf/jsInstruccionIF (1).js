dec0da49ba08418a7defed379e94d3a153c01e99
tellomaxi <40808669+tellomaxi@users.noreply.github.com>
2018-07-10 21:30:45 -0300

    Add files via upload

--- 2-InstruccionIf/jsInstruccionIF (1).js
+++ 2-InstruccionIf/jsInstruccionIF (1).js
@@ -1,7 +1,10 @@
 function mostrar()
 {
 //tomo la edad  
-
-
-
+    var edad
+if (edad < 15 )
+{
+    alert
+}
+    
 }//FIN DE LA FUNCIÓN
--- 2-InstruccionIf/jsInstruccionIF (6).js
+++ 2-InstruccionIf/jsInstruccionIF (6).js
@@ -1,8 +1,21 @@
 function mostrar()
 {
 //tomo la edad  
+var edad;
 
+edad = parseInt(document.getElementById("edad").value);
 
-
-
-}//FIN DE LA FUNCIÓN
+if (edad < 13)
+{
+    alert("Usted es un niño");
+}
+else if(edad >= 13 && edad <= 17)
+{
+    alert("Usted es adolescente");
+}
+else
+{
+    alert("Usted es adulto");
+}
+}
+//FIN DE LA FUNCIÓN
